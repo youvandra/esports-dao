7702cdb4fcadad01bd59873a50fd1a2f47b01152
anasdeyra <anasdoura607@gmail.com>
2023-08-11 11:36:27 +0100

    1 token only

--- src/components/pages/mint/Mint.tsx
+++ src/components/pages/mint/Mint.tsx
@@ -6,9 +6,7 @@ import {
   useAddress,
   useClaimToken,
   useTokenBalance,
-  useWallet,
 } from "@thirdweb-dev/react";
-import { useForm } from "react-hook-form";
 
 interface Inputs {
   amount: number;
@@ -17,21 +15,10 @@ interface Inputs {
 const fileTypes = ["JPG", "PNG"];
 
 export default function Mint() {
-  const {
-    register,
-    formState: { errors },
-    watch,
-  } = useForm<Inputs>();
   const { contract } = useContract(NFT_CONTRACT_ADDRESS);
 
   const { mutateAsync: claimToken } = useClaimToken(contract);
   const address = useAddress();
-
-  const {
-    data: balance,
-    isLoading,
-    error,
-  } = useTokenBalance(contract, address);
 
   return (
     <div className="mt-16">
@@ -39,41 +26,17 @@ export default function Mint() {
         Claim Our Token
       </h2>
       <div className="container mx-auto max-w-4xl flex flex-col gap-4 mt-8">
-        <div>
-          <label htmlFor="amount" className="text-[#ededef] text-xl">
-            Amount
-          </label>
-          <input
-            id="amount"
-            required
-            placeholder="My NFT"
-            type="number"
-            {...register("amount", { valueAsNumber: true })}
-            className={`border-0 ring-1 ring-inset w-full text-[#ededef] ${
-              errors.amount
-                ? "ring-red-600 focus:ring-red-600"
-                : "ring-[#ededef] focus:ring-[#ededef]"
-            } focus:ring-2 focus:ring-inset  px-3 py-5  block   placeholder:text-[#606060] text-xl font-light bg-transparent`}
-          />
-          {balance && (
-            <p className="mt-2 text-[#ededef]">
-              {" "}
-              You currently have {balance.displayValue} tokens
-            </p>
-          )}
-        </div>
-
         <Web3Button
           className="col-span-2 text-xl rounded-none bg-[#ededef]"
           contractAddress={NFT_CONTRACT_ADDRESS}
           action={() =>
             claimToken({
-              amount: watch("amount"),
+              amount: 1,
               to: address ?? "",
             })
           }
         >
-          Mint NFT
+          Claim now
         </Web3Button>
       </div>
     </div>
