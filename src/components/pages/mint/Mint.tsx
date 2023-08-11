8c460d4cbfcc0af3cee2657a12c78fcc3032e52e
anasdeyra <anasdoura607@gmail.com>
2023-08-11 10:48:44 +0100

    added token

--- src/components/pages/mint/Mint.tsx
+++ src/components/pages/mint/Mint.tsx
@@ -1,114 +1,74 @@
 "use client";
 import { NFT_CONTRACT_ADDRESS } from "@/const";
 import {
-  useMintNFT,
   useContract,
   Web3Button,
   useAddress,
+  useClaimToken,
+  useTokenBalance,
+  useWallet,
 } from "@thirdweb-dev/react";
-import { File } from "buffer";
-import { FileUploader } from "react-drag-drop-files";
 import { useForm } from "react-hook-form";
-import { BiImageAdd } from "react-icons/bi";
 
 interface Inputs {
-  name: string;
-  description: string;
-  image: File;
+  amount: number;
 }
 
 const fileTypes = ["JPG", "PNG"];
 
 export default function Mint() {
   const {
-    handleSubmit,
     register,
     formState: { errors },
     watch,
-    setValue,
   } = useForm<Inputs>();
   const { contract } = useContract(NFT_CONTRACT_ADDRESS);
 
-  const handleChange = (file: File | undefined) => {
-    if (!file) return;
-    setValue("image", file);
-  };
-  const { mutateAsync: mintNft, isLoading, error } = useMintNFT(contract);
+  const { mutateAsync: claimToken } = useClaimToken(contract);
   const address = useAddress();
+
+  const {
+    data: balance,
+    isLoading,
+    error,
+  } = useTokenBalance(contract, address);
 
   return (
     <div className="mt-16">
-      <div className="container mx-auto max-w-4xl flex flex-col gap-4">
-        <div className="flex gap-4">
-          <FileUploader
-            className="w-full"
+      <h2 className="mx-auto font-bold text-4xl md:text-5xl w-fit">
+        Claim Our Token
+      </h2>
+      <div className="container mx-auto max-w-4xl flex flex-col gap-4 mt-8">
+        <div>
+          <label htmlFor="amount" className="text-[#ededef] text-xl">
+            Amount
+          </label>
+          <input
+            id="amount"
             required
-            handleChange={handleChange}
-            types={fileTypes}
-          >
-            {watch("image") ? (
-              <img
-                className="object-cover max-h-[360px]"
-                // @ts-ignore
-                src={URL.createObjectURL(watch("image"))}
-              ></img>
-            ) : (
-              <div className="flex flex-col justify-center items-center py-24  border border-[#ededef]">
-                <BiImageAdd color="#606060" size={72} />
-                <span className="text-3xl px-8 font-light text-center text-[#606060]">
-                  Upload Image
-                </span>
-              </div>
-            )}
-          </FileUploader>
-          <div className="flex flex-col gap-4 grow">
-            <div>
-              <label htmlFor="name" className="text-[#ededef] text-xl">
-                Name
-              </label>
-              <input
-                id="name"
-                required
-                placeholder="My NFT"
-                type="text"
-                {...register("name")}
-                className={`border-0 ring-1 ring-inset w-full text-[#ededef] ${
-                  errors.name
-                    ? "ring-red-600 focus:ring-red-600"
-                    : "ring-[#ededef] focus:ring-[#ededef]"
-                } focus:ring-2 focus:ring-inset  px-3 py-5  block   placeholder:text-[#606060] text-xl font-light text-black bg-transparent`}
-              />
-            </div>
-            <div>
-              <label htmlFor="description" className="text-[#ededef] text-xl">
-                Description
-              </label>
-              <textarea
-                id="description"
-                rows={3}
-                required
-                placeholder="This is my NFT"
-                {...register("description")}
-                className={`border-0 ring-1 h-full w-full ring-inset resize-none text-[#ededef] ${
-                  errors.description
-                    ? "ring-red-600 focus:ring-red-600"
-                    : "ring-[#ededef] focus:ring-[#ededef]"
-                } focus:ring-2 focus:ring-inset  px-3 py-5  block   placeholder:text-[#606060] text-xl font-light text-black bg-transparent`}
-              />
-            </div>
-          </div>
+            placeholder="My NFT"
+            type="number"
+            {...register("amount", { valueAsNumber: true })}
+            className={`border-0 ring-1 ring-inset w-full text-[#ededef] ${
+              errors.amount
+                ? "ring-red-600 focus:ring-red-600"
+                : "ring-[#ededef] focus:ring-[#ededef]"
+            } focus:ring-2 focus:ring-inset  px-3 py-5  block   placeholder:text-[#606060] text-xl font-light bg-transparent`}
+          />
+          {balance && (
+            <p className="mt-2 text-[#ededef]">
+              {" "}
+              You currently have {balance.displayValue} tokens
+            </p>
+          )}
         </div>
 
         <Web3Button
-          className="col-span-2 rounded-none bg-[#ededef]"
+          className="col-span-2 text-xl rounded-none bg-[#ededef]"
           contractAddress={NFT_CONTRACT_ADDRESS}
           action={() =>
-            mintNft({
-              metadata: {
-                name: watch("name"),
-                description: watch("description"),
-                image: watch("image"),
-              },
+            claimToken({
+              amount: watch("amount"),
               to: address ?? "",
             })
           }
--- src/const.ts
+++ src/const.ts
@@ -2,4 +2,4 @@ export const EMAIL_SENDER = "esportsdao.sale@gmail.com";
 export const EMAIL_RECEIVER = "esportsdao.get@gmail.com";
 export const THIRDWEB_CLIENT_ID = "78947c60fa7af932b0406080c5d7e944" as const;
 export const NFT_CONTRACT_ADDRESS =
-  "0x8D9bEb2a7D3bE1A0cC3a1791bDf31B3704fB2E7b" as const;
+  "0xA6FCaAba50672d8E5a6Fb7480721dAdCdB0dcE26" as const;
