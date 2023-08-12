0ccb28d3cf47237dea2b55d1b5e37e87eeb6ad3d
unknown <kucingplaygame@gmail.com>
2023-08-12 14:50:23 +0700

    change token

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
 export const EMAIL_SENDER = "esportsdao.sale@gmail.com";
 export const EMAIL_RECEIVER = "esportsdao.get@gmail.com";
-export const THIRDWEB_CLIENT_ID = "78947c60fa7af932b0406080c5d7e944" as const;
+export const THIRDWEB_CLIENT_ID = "55aed8472e08d73f0e517a6b1568f91a" as const;
 export const NFT_CONTRACT_ADDRESS =
-  "0xA6FCaAba50672d8E5a6Fb7480721dAdCdB0dcE26" as const;
+  "0x8BA80e7cedaF068418E848cee91c4E7D59f5Bd04" as const;
