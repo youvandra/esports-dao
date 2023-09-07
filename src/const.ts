e41212052fb637fb4fbf1ef81808a6448abdd26d
unknown <kucingplaygame@gmail.com>
2023-09-07 08:24:02 +0700

    client-id

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export const EMAIL_SENDER = "esportsdao.sale@gmail.com";
 export const EMAIL_RECEIVER = "esportsdao.get@gmail.com";
-export const THIRDWEB_CLIENT_ID = "55aed8472e08d73f0e517a6b1568f91a" as const;
+export const THIRDWEB_CLIENT_ID = "cad674a079c4514cbbd28533f6b08686" as const;
 export const NFT_CONTRACT_ADDRESS =
   "0xe64B0aA100e6Ef0fF23FeAD9d1c3b0c766d6A2cc" as const;
 export const POLYGON_CONTRACT_ADDRESS = "" as const;
